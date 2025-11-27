6252e1227a773580190c855f1d3522b2ceadf4cc
Lance Kian Flores <lancekian12@gmail.com>
2025-11-28 02:07:35 +0800

    create a redux logger middleware to display the state much better

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const store = require("./app/store");
-const { cakeActions } = require('./features/cake/cakeSlice')
-const { iceCreamActions } = require('./features/icecream/iceCreamSlice')
+const { cakeActions } = require("./features/cake/cakeSlice");
+const { iceCreamActions } = require("./features/icecream/iceCreamSlice");
 
 console.log("Initial state", store.getState());
-const unsubscribe = store.subscribe(() => {
-  console.log("Updated state", store.getState());
-});
+const unsubscribe = store.subscribe(() => {});
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(10));
 
-store.dispatch(iceCreamActions.ordered())
-store.dispatch(iceCreamActions.ordered())
-store.dispatch(iceCreamActions.restocked(5))
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.restocked(5));
 
 unsubscribe();
